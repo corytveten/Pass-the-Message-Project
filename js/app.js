35fb1ed0521d1f120573b8859829e3c3c0406b77
corytveten <corytveten@gmail.com>
2020-06-27 11:29:30 -0700

    solution

--- js/app.js
+++ js/app.js
@@ -1,28 +1,13 @@
 
-(function() {
-//Select the input element
-const form = document.querySelector('#message-form')
-//Set up Submit Button
-form.addEventListener('submit', function(e){
-    // prevent the form's default submission action
+const submitBtn = document.getElementById('submitBtn');
+const message = document.querySelector('.message-content');
+const input = document.getElementById('message');
+
+
+function printMessage(e) {
     e.preventDefault()
-    //Get user's input from from
-    const message = document.querySelector('#message')
-    const feedback = document.querySelector('.feedback')
-    const messageContent = document.querySelector('.message-content')
+    message.textContent = input.value;
+}
 
-    if (message.value === ''){
-        feedback.classList.add('show')
-        setTimeout(function(){
-        feedback.classList.remove('show')
-        }, 2000)
-    } else {
-        //Change message content and clear the message input
-        messageContent.textContent = message.value
-        message.value = ''
-    }
-})
-})()
-
-
+document.addEventListener('submit', printMessage)
 
